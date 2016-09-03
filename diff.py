b48777df7ab12777632c750b22d13e3a9d7536a9
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2016-09-04 00:50:39 +0200

    added basic args handling

--- diff.py
+++ diff.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 #-*- encoding: utf-8 -*-
 
+import argparse
+
+def main(src_index_url, dest_index_url):
+    print('ok')
+
 if __name__ == "__main__":
-    pass
+    argparser = argparse.ArgumentParser(description='elasticdiff')
+    argparser.add_argument('src_index_url')
+    argparser.add_argument('dest_index_url')
+    args = argparser.parse_args()
+    main(args.src_index_url, args.dest_index_url)
